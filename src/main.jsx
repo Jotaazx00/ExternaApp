e4fc1b5247483678b43c76a12d4dbbc703fcb7b1
Jotaazx00 <joaquimrosa.extsj@gmail.com>
2026-09-09 23:09:30 -0300

    Adiciona src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App.jsx";
+import "./design.css";
+import "./polish.css";
+import "./final.css";
+import "./learning.css";
+import "./evi.css";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => navigator.serviceWorker.register("/sw.js").catch(() => {}));
+}
+
+
+
+
